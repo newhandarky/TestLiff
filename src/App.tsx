77e473065baab0ff2b599428dd1a502077e7f6bb
Atsushi <newhandarky@gmail.com>
2025-02-11 16:22:40 +0800

    測試改用HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Home from "../src/pages/Home";
 import Info from "../src/pages/Info";
@@ -10,13 +10,13 @@ function App() {
 
   return (
     <>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Info" element={<Info />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   )
 }
